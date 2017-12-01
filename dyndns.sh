ba6fb565af3e6aabd7889c2f4d7bc979787ad8ab
thearchdukeofdoge <jeffkaminski98@yahoo.com>
2017-11-30 21:12:35 -0500

    Fixed an issue with the updates running only when the first record checked needed updating
    Corrected the logic so that each record is checked each time
    Made the new ip and current dns if loop actually work
    
    Added logic so that when the %40 record (@) is checked the dig just runs the base domain, because you can't dig @.example.com

--- dyndns.sh
+++ dyndns.sh
@@ -6,7 +6,7 @@
 #
 #Declare your variables################################
 #Define your api key (password)
-api_key=<get_your_own_api_key>
+api_key=<get)your_own_api_key>
 #
 #Your domain name
 domain_name=example.com
@@ -18,26 +18,28 @@ records=(%40 www) #The %40 is a url encoded @ symbol
 
 #Function to send the new ipaddress to Namecheap
 function update_ip {
-	for record in `echo ${records[*]}`;
-	do
 	#q for quiet, spider for no download
-	wget -q --spider https://dynamicdns.park-your-domain.com/update\?host\=$record\&domain\=$domain_name\&password\=$api_key\&ip\=$new_ip
+	wget -q --spider https://dynamicdns.park-your-domain.com/update\?host\=$1\&domain\=$domain_name\&password\=$api_key\&ip\=$new_ip
 	#Wait a polite amount of time between record updates
 	sleep 10
-	done
 }
 
 #New ip address, finds your current external ip address
 new_ip=$(curl -s httpbin.org/ip | grep -i origin | sed 's/  \"origin\"\: \"//' | sed 's/\"//')
 
-#Current dns record
-current_dns=$(dig $domain_name +short)
+for record in `echo ${records[*]}`;
+	do
+	#Current dns record
+	if [ $record = "%40" ]
+	then
+		current_dns=$(dig $domain_name +short)
+	else
+		current_dns=$(dig $record.$domain_name +short)
+	fi
 
-#Does the domain need to be updated?
-if [ new_ip != current_dns ]
-    then
-    update_ip
-    else
-    exit #nothing to do
-fi
-
+	#Does the domain need to be updated?
+	if [ "$new_ip" != "$current_dns" ]
+    	then
+    	update_ip $record
+	fi
+done
